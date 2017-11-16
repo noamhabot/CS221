abb4759a7710156f4db30fda1252185a4deb9950
Mackenzie Pearson <mackenziejpearson@gmail.com>
2017-11-15 22:59:29 -0800

    Minor edit to model.R added preliminary_error.csv

--- Code/model.R
+++ Code/model.R
@@ -5,7 +5,7 @@ library(caTools)
 #data = data.table(read.csv("~/Downloads/dataset.csv"), sep = ',', header = FALSE, fill = TRUE)
 #saveRDS(data, "~/Documents/cs221/CS221FinalProject/data.rds")
 #data = readRDS("~/Documents/cs221/CS221FinalProject/data.rds")
-data_ = subset(data, select=c(2,4,5,6,7,8,9,10,11,12:30))
+data_ = subset(data, select=c(2,4,5,6,7,8,9,10,11,12:36))
 colnames(data_)
 
 # 2/3 of observations dont have a region 2
@@ -25,28 +25,32 @@ data_s = na.omit(data_)
 countries = tail(names(sort(table(data_s$country))), 15)
 provinces = tail(names(sort(table(data_s$province))), 50) 
 regions = tail(names(sort(table(data_s$region_1))), 50) 
-varieties = tail(names(sort(table(data_s$variety))), 200)
+varieties = tail(names(sort(table(data_s$variety))), 100)
 # wineries = tail(names(sort(table(data_s$winery))), 1500)
 
 data_s = data_s[country %in% countries & province %in% provinces & region_1 %in% regions & variety %in% varieties,]
 
-sample = sample.int(n = nrow(data_s), size = floor(.8*nrow(data_s)), replace = F)
-train = data[sample, ]
-test  = data[-sample, ]
+# sample = sample.int(n = nrow(data_s), size = floor(.8*nrow(data_s)), replace = F)
+# train = data[sample, ]
+# test  = data[-sample, ]
 
-# split = sample.split(data_s$flavors, SplitRatio = 0.8)
-# train = subset(data_s, split == TRUE)
-# test = subset(data_s, split == FALSE)
+split = sample.split(data_s$flavors, SplitRatio = 0.8)
+train = subset(data_s, split == TRUE)
+test = subset(data_s, split == FALSE)
 
-range = 7:25
+range = 7:31
 error = rep(0, length(range))
 for (i in range) {
   thisVariable <- names(train)[i]
   model = glm (train[, get(thisVariable)] ~ points+price+country+province+region_1+variety, data = train, family = binomial)
   err = 1 - sum((predict(model, newdata = test, type = "response") > 0.5) == (test[, get(thisVariable)] > 0.5))/nrow(test)
-  error[i] = err
+  error[i-6] = err
+  print(i-6)
   print(thisVariable)
 }
+
+preliminary_error = data.frame(names(train)[range], error)
+write.csv(preliminary_error, "~/Documents/cs221/CS221FinalProject/preliminary_error.csv")
 
 for (i in range) {
   cat("Prediciton error for \"",  names(train)[i],"\" on the test dataset is ", round(error[i],2), "\n", sep ="")
