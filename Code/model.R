971baf1f94101fd22d1322c5bb8df3864d5a6850
Noam Habot <noam.habot@gmail.com>
2017-12-06 14:45:08 -0800

    finna add more to ROC

--- Code/model.R
+++ Code/model.R
@@ -44,70 +44,84 @@ test = subset(data_s, split == FALSE)
 
 ###### METRICS NEW ########
 ###########################
-n = 100#nrow(prediction)
+prediction <- readRDS("~/Google Drive/Stanford/Stanford Y2 Q1/CS 221/CS221FinalProject/Predictions/predictions_108_357.rds")
+
+
+n = 10#nrow(prediction)
+randomSample <- sample(1:nrow(prediction), n)
 m = ncol(prediction)
-startIndex <- 7 # by default, this is 7
-recall = rep(0, n)
-precision = rep(0, n)
-for (i in 1:n) {
-  added_probability = 0.2
-  thisObsKeywords = test[i, startIndex:(m+startIndex-1)]
-  predictions <- round(prediction[i, 1:m]+added_probability)
-  acc = table(Predictions = as.numeric(predictions), TrueLabels = as.numeric(thisObsKeywords))
-  TN = acc[1]
-  FP = acc[2]
-  FN = acc[3]
-  TP = acc[4]
-  precision[i] = TP / (TP + FP)
-  recall[i] = TP / (TP + FN)
+startIndex <- 108 # by default, this is 7
+
+recallMeans <- c()
+specificityMeans <- c()
+Fscores <- c()
+for (added_probability in c(seq(-0.5, 0.4, 0.1), seq(0.4, 0.495, 0.005), seq(0.495, 0.5, 0.001))) {
+  print(added_probability)
+
+  recall = rep(0, n)
+  precision = rep(0, n)
+  specificity = rep(0, n)
+  for (i in 1:n) {
+    
+    thisObsKeywords = test[randomSample[i], startIndex:(m+startIndex-1)]
+    predictions <- round(prediction[randomSample[i], 1:m]+added_probability)
+    acc = table(Predictions = as.numeric(predictions), TrueLabels = as.numeric(thisObsKeywords))
+    TN = acc[1]
+    FP = acc[2]
+    FN = acc[3]
+    TP = acc[4]
+    
+    if (nrow(acc) == 1) {
+      if (rownames(acc)[1] == "1") {
+        TN = 0
+        FP = acc[1]
+        FN = 0
+        TP = acc[2]
+      } else if (rownames(acc)[1] == "0") {
+        TN = acc[1]
+        FP = 0
+        FN = acc[2]
+        TP = 0
+      }
+
+    }
+    if (TP == 0) {
+      precision[i] = 0
+    } else {
+      precision[i] = TP / (TP + FP)
+    }
+    recall[i] = TP / (TP + FN)
+    specificity[i] = TN/(TN+FP)
+  }
+  
+  recall[is.nan(recall)]=NA
+  precision[is.nan(precision)]=NA
+  specificity[is.nan(specificity)]=NA
+  
+  recallScore = mean(recall, na.rm = TRUE)
+  precisionScore = mean(precision, na.rm=TRUE)
+  specificityScore = mean(specificity, na.rm=TRUE)
+  
+  Fscore = (2*precisionScore*recallScore)/(precisionScore+recallScore)
+  Fscores <- c(Fscores, Fscore)
+  
+  print(paste("Mean of recall:", recallScore))
+  print(paste("Mean of precision:", precisionScore))
+  print(paste("Mean of specificity:", specificityScore))
+
+  recallMeans <- c(recallMeans, recallScore)
+  specificityMeans <- c(specificityMeans, specificityScore)
 }
-
-###### METRICS ########
-#######################
-totalPredictions <- readRDS("~/Google Drive/Stanford/Stanford Y2 Q1/CS 221/CS221FinalProject/Predictions/predictions_totalfirst1000.rds")
-prediction = totalPredictions
-n = 100#nrow(prediction)
-m = ncol(prediction)
-startIndex <- 7 # by default, this is 7
-recall = rep(0, n)
-precision = rep(0, n)
-for (i in 1:n) {
-  print(paste("Iteration:", i, "out of", n))
-  
-  # metric 1
-  in_review = 0
-  in_prediction = 0
-  thisObsKeywords = test[i, startIndex:(m+startIndex-1)]
-  predictions <- round(prediction[i, 1:m]+0.2)
-  
-  in_review = sum(thisObsKeywords)
-  in_prediction = sum(bitwAnd(as.numeric(thisObsKeywords), as.numeric(predictions)))
-  
-  recall[i] = in_prediction / in_review
-  
-  # metric 2
-  in_prediction = 0
-  not_in_review = 0
-  
-  in_prediction = sum(predictions)
-  not_in_review = as.numeric(table(as.numeric(predictions)-as.numeric(thisObsKeywords))["1"])
-  
-  precision[i] = 1-(not_in_review / in_prediction)
-}
-recall[is.nan(m1)]=NA
-precision[is.nan(m2)]=NA #### WAS WAS WAS m2_test[is.nan(m1)]=NA
-
-recallScore = mean(recall, na.rm = TRUE)
-precisionScore = mean(precision, na.rm=TRUE)
-
-Fscore = (2*precisionScore*recallScore)/(precisionScore+recallScore)
-
-print(paste("Mean of recall:", recallScore))
-print(paste("Mean of precision:", precisionScore))
+FPR <- (1-specificityMeans)
+plot(recallMeans ~ FPR, xlim=c(0,1), ylim=c(0,1))
+abline(0,1)
+plot(Fscores[2:37])
+#ggplot this bitch
 
 
 
-
+FPR100 <- FPR
+recall100 <- recallMeans
 
 
 
